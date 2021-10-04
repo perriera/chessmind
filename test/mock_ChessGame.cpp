44c283138eed9c1242cd98f5fd4bb574c91ff068
Perry Anderson <perry.anderson@gmail.com>
2021-10-04 14:35:36 -0700

    Refactored project

--- test/mock_ChessGame.cpp
+++ test/mock_ChessGame.cpp
@@ -1,19 +1,19 @@
 
 #include <iostream>
-#include "../include/cpp_starter/game/ChessGame.hpp"
 
-#include "vendor/catch.hpp"
-#include "vendor/fakeit.hpp"
+#include "catch.hpp"
+#include "../include/chessmind/game/ChessGame.hpp"
+#include "fakeit.hpp"
 
 using namespace extras;
 using namespace fakeit;
 
 SCENARIO("Mock ChessGameInterface: toOctal", "[CHES-9]") {
-
+    ChessMoves correct_answer;
     Mock<ChessGameInterface> mock;
-    When(Method(mock, moves)).Return();
+    When(Method(mock, moves)).Return(correct_answer);
 
     ChessGameInterface& i = mock.get();
-    i.moves();
+    REQUIRE(i.moves() == correct_answer);
     Verify(Method(mock, moves));
 }
